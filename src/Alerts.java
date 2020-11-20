acedd1344e3f30a5bf01bf6d2d6f753e66711ca0
harish@gmail.com <harish.jaladi188@gmail.com>
2020-11-21 03:12:56 +0530

    Alerts Program

---
+++ src/Alerts.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\harish\\Desktop\\Selenium course\\Needed Softwares\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("name")).sendKeys("Harish");
+		driver.findElement(By.id("alertbtn")).click();
+		//driver.switchTo().alert().accept();
+		
+		//driver.findElement(By.id("name")).sendKeys("Harish");
+		//driver.findElement(By.id("confirmbtn")).click();
+		//driver.switchTo().alert().dismiss();
+		driver.switchTo().alert().accept();
+		System.out.println(driver.switchTo().alert().getText());
+
+		
+
+		
+	}
+
+}
