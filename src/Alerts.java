5525b52293f34e0e6f4c818bcad92d85fbf37e00
harish <harish.jaladi188@gmail.com>
2020-11-22 19:22:10 +0530

    today

--- src/Alerts.java
+++ src/Alerts.java
@@ -16,6 +16,7 @@ public class Alerts {
 		System.out.println("test1");
 		System.out.println("test2");
 		System.out.println("test3");
+		System.out.println("test3");
 		//driver.findElement(By.id("name")).sendKeys("Harish");
 		//driver.findElement(By.id("confirmbtn")).click();
 		//driver.switchTo().alert().dismiss();
